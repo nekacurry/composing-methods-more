9828250a3487e652c55fc36486a670d499c5723b
nekacurry <anneka498@gmail.com>
2022-03-04 23:01:44 -0600

    Initial commit

---
+++ burger_toppings.py
@@ -0,0 +1,18 @@
+# by Kami Bigdely
+# Split temporary variable
+
+patty = 2 * 70 # [gr]
+pickle = 4 * 20 # [gr]
+tomatoes = 3 * 25 # [gr]
+lettuce = 2 * 15 # [gr]
+buns = 2 * 95 # [gr]
+kimchi = 30 # [gr]
+mayo = 5 # [gr]
+golden_fried_onion = 20 # [gr]
+
+sandwich_weight = (patty + pickle + tomatoes + lettuce + buns)
+print("NY Burger Weight", sandwich_weight)
+
+seoul_sandwich_weight = (patty +  pickle + tomatoes + kimchi + mayo + golden_fried_onion + buns)
+print("Seoul Kimchi Burger Weight", seoul_sandwich_weight)
+
